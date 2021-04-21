dd9591f1d99faebf4f81efca5cf14d6e4ab492b2
2Zero1 <2Zero1@users.noreply.github.com>
2021-04-21 07:26:56 +0000

    deploy: 174bc814ae97d04b6e37ba1913896b4ca2bb86b8

--- main.js
+++ main.js
@@ -1474,7 +1474,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst messages = {\n  secretMessage: {\n    wrong: '올바른 메시지를 입력해주세요',\n    default: '엽서 암호 길이는 5자 ~ 15자 입니다.',\n  },\n  sender: {\n    default: '이름 길이는 2자 ~ 5자 입니다.',\n  },\n  receiver: {\n    default: '이름 길이는 2자 ~ 5자 입니다.',\n  },\n  contents: {\n    default: '내용의 길이는 30 ~ 200자 입니다.',\n  },\n  photoMessage: {\n    default: '사진 메시지 길이는 10자 ~ 30자 입니다.',\n  },\n  photo: {\n    default: '이미지를 선택해주세요.',\n  },\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (messages);\n\n\n//# sourceURL=webpack://postcard/./src/text/errorMessages.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst messages = {\n  secretMessage: {\n    wrong: '올바른 엽서 암호를 입력해주세요',\n    default: '엽서 암호 길이는 5자 ~ 15자 입니다.',\n  },\n  sender: {\n    default: '이름 길이는 2자 ~ 5자 입니다.',\n  },\n  receiver: {\n    default: '이름 길이는 2자 ~ 5자 입니다.',\n  },\n  contents: {\n    default: '내용의 길이는 30 ~ 200자 입니다.',\n  },\n  photoMessage: {\n    default: '사진 메시지 길이는 10자 ~ 30자 입니다.',\n  },\n  photo: {\n    default: '이미지를 선택해주세요.',\n  },\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (messages);\n\n\n//# sourceURL=webpack://postcard/./src/text/errorMessages.js?");
 
 /***/ }),
 
